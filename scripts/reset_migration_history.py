a4fd22bbbb0fcd2dc939ce39236f916776d181b5
bradyespey <baespey@gmail.com>
2024-11-10 09:24:31 +1100

    Updated migrations for Heroku deployment

---
+++ scripts/reset_migration_history.py
@@ -0,0 +1,14 @@
+import sys
+from pathlib import Path
+
+# Add the project root directory to sys.path
+sys.path.append(str(Path(__file__).resolve().parent.parent))
+
+from app import app, db
+from sqlalchemy import text
+
+# Ensure actions are within the application context
+with app.app_context():
+    with db.engine.connect() as conn:
+        conn.execute(text("DROP TABLE IF EXISTS alembic_version;"))
+        print("Cleared alembic_version table.")
