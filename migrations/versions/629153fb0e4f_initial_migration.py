af9984a2a5701d05df280a90ad529b0ef1c309b3
bradyespey <baespey@gmail.com>
2024-11-26 08:57:14 -0600

    Initial migration setup

---
+++ migrations/versions/629153fb0e4f_initial_migration.py
@@ -0,0 +1,24 @@
+"""Initial migration
+
+Revision ID: 629153fb0e4f
+Revises: 
+Create Date: 2024-11-26 08:55:54.584964
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '629153fb0e4f'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
